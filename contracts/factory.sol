f06c040acadd3f8c596d59b43f42ca9530f6eeba
Oluwatomilola <tommy4gold@gmail.com>
2025-08-13 13:51:52 +0100

    Updated factory.sol

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -6,20 +6,20 @@ import "./TomiNFT.sol";
 contract Factory {
    TomiNFT[] public TomiNFTArray;
 
-  // function mintNFT(address recipient, string memory tokenURI)
-  //       public
-  //       onlyOwner
-  //       returns (uint256)
-  //   {
-  //       require(recipient != address(0), "Recipient cannot be zero address");
-  //       require(bytes(tokenURI).length > 0, "Token URI cannot be empty");
+  function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        require(recipient != address(0), "Recipient cannot be zero address");
+        require(bytes(tokenURI).length > 0, "Token URI cannot be empty");
 
-  //       _tokenIds = 1; // Set to 1 for single NFT
-  //       uint256 newItemId = _tokenIds;
-  //       _safeMint(recipient, newItemId);
-  //       _setTokenURI(newItemId, tokenURI);
+        _tokenIds = 1; // Set to 1 for single NFT
+        uint256 newItemId = _tokenIds;
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-  //       emit NFTMinted(recipient, newItemId, tokenURI);
-  //       return newItemId;
-  //   }
+        emit NFTMinted(recipient, newItemId, tokenURI);
+        return newItemId;
+    }
 }
